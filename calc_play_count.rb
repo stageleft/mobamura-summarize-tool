3218ed8223a6e36a37ebacb151e8194b6e3dd319
stageleft <sugano-github@elder-alliance.org>
2022-06-30 17:41:06 +0900

    summarize to CN->Name->Count list （出力γ）

---
+++ calc_play_count.rb
@@ -0,0 +1,53 @@
+require 'json'
+
+class CalcPlayCount
+    def initialize
+    end
+    def set_player_name(queryresult)
+        ret_value = {}
+
+        queryresult.each_key{|k|
+            ret_value[k] = []
+            queryresult[k].each{|l|
+                if l["HN"] == "-" then
+                    ret_value[k].push(l["trip"])
+                else
+                    ret_value[k].push(l["HN"])
+                end
+            }
+        }
+
+        ret_value
+    end
+    def count_play(cnlist, queryresult)
+        ret_value = {} # ret_value.character.player: int
+
+        l = JSON.parse(cnlist)
+        l["characteres"].each{|e|
+            if ret_value[e] == nil then
+                ret_value[e] = {}
+            end
+            queryresult[e].each{|f|
+                if ret_value[e][f] == nil then
+                    ret_value[e][f] = 1
+                else
+                    ret_value[e][f] = ret_value[e][f] + 1
+                end
+            }
+        };
+        l["alias"].each{|e|
+            if ret_value[e["link"]] == nil then
+                ret_value[e["link"]] = {}
+            end
+            queryresult[e["cn"]].each{|f|
+                if ret_value[e["link"]][f] == nil then
+                    ret_value[e["link"]][f] = 1
+                else
+                    ret_value[e["link"]][f] = ret_value[e["link"]][f] + 1
+                end
+            }
+        };
+
+        ret_value
+    end
+end
--- index.rb
+++ index.rb
@@ -1,4 +1,5 @@
 require './get_player_list'
+require './calc_play_count'
 
 if ARGV.size != 1 then
     puts "usage : ruby index.rb [character_list.json]"
@@ -6,11 +7,15 @@ if ARGV.size != 1 then
 end
 
 mjlinstance = GetPlayerList.new
+mjlcounter  = CalcPlayCount.new
+
 File.open(ARGV[0]){|f|
     rawdata = f.readlines
     jsondata = rawdata.join('')
     raw_result = mjlinstance.queryByJson(jsondata)  # hash result of {CN=>[{hn=>hn, trip=>trip},...], CN=>...}
 
-    puts raw_result # debug
+    calced_result = mjlcounter.count_play(jsondata, mjlcounter.set_player_name(raw_result))
+
+    puts calced_result # debug
     # TODO: calc raw_result and get 
 }
